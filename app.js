40f6aebd55693a4a0d30e9bdae6f66dde3676ad8
Daniel Lima <yourwebmaker@gmail.com>
2026-02-24 16:27:01 -0300

    Fix api endpoint

--- app.js
+++ app.js
@@ -18,21 +18,25 @@ app.get('/', (req, res) => {
 app.post("/api/postcodecheck", async (req, res) => {
   try {
     const payload = {
-      postalcode: String(req.query.postalCode ?? req.query.postalcode ?? "").replace(/\s+/g, "").toUpperCase(),
-      housenumber: String(req.query.houseNumber ?? req.query.housenumber ?? ""),
-      housenumberaddition: String(req.query.houseNumberAddition ?? req.query.housenumberaddition ?? ""),
+      postalcode: String(req.body.postalCode ?? req.body.postalcode ?? "").replace(/\s+/g, "").toUpperCase(),
+      housenumber: String(req.body.houseNumber ?? req.body.housenumber ?? ""),
+      housenumberaddition: String(req.body.houseNumberAddition ?? req.body.housenumberaddition ?? ""),
     };
 
-    const r = await fetch(
-      "https://api.postnl.nl/shipment/checkout/v1/postalcodecheck?postalcode=" + payload.postalcode + "&housenumber=" + payload.housenumber + "&housenumberaddition=" + payload.housenumberaddition,
+    const BASE_URL = "https://api.postnl.nl/v2/address/benelux";
+    const url = new URL(BASE_URL);
+    url.searchParams.set("postalCode", payload.postalcode);
+    url.searchParams.set("houseNumber", parseInt(payload.housenumber) || 0);
+    url.searchParams.set("countryIso", 'NL');
+
+    const r = await fetch(url.toString(),
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
           apikey: process.env.POSTNL_API_KEY,
           Accept: "application/json",
-        },
-        body: JSON.stringify(payload),
+        }
       }
     );
 
@@ -44,3 +48,6 @@ app.post("/api/postcodecheck", async (req, res) => {
 });
 
 app.listen(8080, () => console.log("Proxy running on http://127.0.0.1:8080"));
+
+// Export the Express app
+export default app;
